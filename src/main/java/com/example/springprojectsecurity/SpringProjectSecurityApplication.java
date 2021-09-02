77af0ba5ab60a9d78150f8ea6c64a74b6bac09da
seonb2n <626ksb@gmail.com>
2021-09-02 16:24:11 +0900

    pjt create

---
+++ src/main/java/com/example/springprojectsecurity/SpringProjectSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springprojectsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springprojectsecurity/SpringProjectSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springprojectsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
