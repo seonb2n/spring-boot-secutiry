b823d750f0695de298c0136272de60939be5bdc5
seonb2n <626ksb@gmail.com>
2021-09-02 18:37:37 +0900

    spring secutiry 개념 학습

--- server/basic-test/src/main/java/com/sp/fc/web/BasicTestApplication.java
+++ server/basic-test/src/main/java/com/sp/fc/web/BasicTestApplication.java
@@ -1,4 +1,4 @@
-package com.sp.fc;
+package com.sp.fc.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BasicTestApplication {
 
     public static void main(String[] args) {
-
-        Person person = null;
 
         SpringApplication.run(BasicTestApplication.class, args);
     }
---
+++ server/basic-test/src/main/java/com/sp/fc/web/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.sp.fc.web.controller;
+
+import org.springframework.security.access.SecurityMetadataSource;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "홈페이지";
+    }
+
+    @RequestMapping("/auth")
+    public  Authentication authentication() {
+        return SecurityContextHolder.getContext()
+                .getAuthentication();
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_USER')")
+    @RequestMapping("/user")
+    public SecurityMessage user() {
+        return SecurityMessage.builder()
+                .auth(SecurityContextHolder.getContext().getAuthentication())
+                .message("User 정보")
+                .build();
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    @RequestMapping("/admin")
+    public SecurityMessage admin() {
+        return SecurityMessage.builder()
+                .auth(SecurityContextHolder.getContext().getAuthentication())
+                .message("Admin 정보")
+                .build();
+    }
+
+}
