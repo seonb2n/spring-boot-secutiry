631d5e6d20120192a38d9fa7f8d8e278f7c4ac5b
seonb2n <626ksb@gmail.com>
2021-09-04 18:54:22 +0900

    student, teacher 만들어서 각기 login 되고 다른 권한 부여하도록 학습

--- server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,25 +1,31 @@
 package com.sp.fc.web.config;
 
 import com.sp.fc.web.student.StudentManager;
+import com.sp.fc.web.teacher.TeacherManager;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final StudentManager studentManager;
+    private final TeacherManager teacherManager;
 
-    public SecurityConfig(StudentManager studentManager) {
+    public SecurityConfig(StudentManager studentManager, TeacherManager teacherManager) {
         this.studentManager = studentManager;
+        this.teacherManager = teacherManager;
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(studentManager);
+        auth.authenticationProvider(teacherManager);
     }
 
     @Override
@@ -32,6 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin(
                     login->login.loginPage("/login").permitAll()
             )
+            .exceptionHandling(e -> e.accessDeniedPage("/access-denied"))
                 ;
     }
 
--- server/login-custom-filter/src/main/java/com/sp/fc/web/teacher/TeacherManager.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/teacher/TeacherManager.java
@@ -1,5 +1,7 @@
-package com.sp.fc.web.student;
+package com.sp.fc.web.teacher;
 
+import com.sp.fc.web.student.Student;
+import com.sp.fc.web.student.StudentAuthenticationToken;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,18 +14,18 @@ import java.util.HashMap;
 import java.util.Set;
 
 @Component
-public class StudentManager implements AuthenticationProvider, InitializingBean {
+public class TeacherManager implements AuthenticationProvider, InitializingBean {
 
-    private HashMap<String, Student> studentDB = new HashMap<>();
+    private HashMap<String, Teacher> teacherDB = new HashMap<>();
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
-        if(studentDB.containsKey(token.getName())) {
-            Student student = studentDB.get(token.getName());
-            return StudentAuthenticationToken.builder()
-                    .principal(student)
-                    .details(student.getUsername())
+        if(teacherDB.containsKey(token.getName())) {
+            Teacher teacher = teacherDB.get(token.getName());
+            return TeacherAuthenticationToken.builder()
+                    .principal(teacher)
+                    .details(teacher.getUsername())
                     .authenticated(true)
                     .build();
         }
@@ -41,9 +43,9 @@ public class StudentManager implements AuthenticationProvider, InitializingBean 
     public void afterPropertiesSet() throws Exception {
         //Bean 이 초기화 될때 작동.
         Set.of(
-                new Student("hong", "홍진호", Set.of(new SimpleGrantedAuthority("ROLE_STUDENT"))),
-                new Student("kim", "김택용", Set.of(new SimpleGrantedAuthority("ROLE_STUDENT"))),
-                new Student("lee", "이영호", Set.of(new SimpleGrantedAuthority("ROLE_STUDENT")))
-        ).forEach(s -> studentDB.put(s.getId(), s));
+                new Teacher("lim", "임요한", Set.of(new SimpleGrantedAuthority("ROLE_TEACHER"))),
+                new Teacher("song", "송병구", Set.of(new SimpleGrantedAuthority("ROLE_TEACHER"))),
+                new Teacher("park", "박성준", Set.of(new SimpleGrantedAuthority("ROLE_TEACHER")))
+        ).forEach(s -> teacherDB.put(s.getId(), s));
     }
 }
