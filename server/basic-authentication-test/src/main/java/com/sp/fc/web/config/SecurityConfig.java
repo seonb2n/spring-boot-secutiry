8d269b6db743a98c27db47837804449ded5d9da8
seonb2n <626ksb@gmail.com>
2021-09-05 10:44:54 +0900

    basic 인증 학습을 위한 세팅2

--- server/basic-authentication-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/basic-authentication-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().disable() //post 방식은 csrf 인증을 필요로 한다.
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .httpBasic(); //basic Authentication filter 를 사용하기 위한 옵션 추가
---
+++ server/login-multi-chain/src/test/java/com/sp/fc/web/MultiChainProxyTest.java
@@ -0,0 +1,8 @@
+package com.sp.fc.web;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MultiChainProxyTest {
+
+}
