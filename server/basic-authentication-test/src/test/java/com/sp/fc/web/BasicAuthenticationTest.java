56784a0fe839ffc85ab4942669a350b1e1d55805
seonb2n <626ksb@gmail.com>
2021-09-05 10:01:16 +0900

    basic authentication filter 작동하는 방법 학습

--- server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
+++ server/basic-authentication-test/src/test/java/com/sp/fc/web/BasicAuthenticationTest.java
@@ -4,8 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BasicAuthenticationTest {
@@ -23,9 +35,43 @@ public class BasicAuthenticationTest {
     @Test
     void test_1() {
 
-        String response = client.getForObject(greetingUrl(), String.class);
-        System.out.println(response);
+        HttpClientErrorException exception = assertThrows(HttpClientErrorException.class, ()-> {
+            client.getForObject(greetingUrl(), String.class);
+        });
 
+        assertEquals(401, exception.getRawStatusCode());
+
+    }
+
+    @DisplayName("2. 인증 성공")
+    @Test
+    void test_2() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString(
+                "user1:1111".getBytes()
+        ));
+        HttpEntity entity = new HttpEntity(null, headers);
+        ResponseEntity<String> resp = client.exchange(greetingUrl(), HttpMethod.GET, entity, String.class);
+
+        assertEquals("hello", resp.getBody());
+
+//        System.out.println(resp.getBody());
+    }
+
+    @DisplayName("3. 인증 성공2")
+    @Test
+    void test_3() {
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        String resp = testClient.getForObject(greetingUrl(), String.class);
+        assertEquals("hello", resp);
+    }
+
+    @DisplayName("4. POST 인증")
+    @Test
+    void test_4() {
+        TestRestTemplate testClient = new TestRestTemplate("user1", "1111");
+        ResponseEntity<String> resp = testClient.postForEntity(greetingUrl(), "jake", String.class);
+        assertEquals("hello jake", resp.getBody());
     }
     
 }
