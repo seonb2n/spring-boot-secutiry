70689050ec04eb82c676ccd0566c6f7855acb7dc
seonb2n <626ksb@gmail.com>
2021-09-08 21:15:40 +0900

    remember me service 학습

--- server/login-rememberme/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-rememberme/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sp.fc.web.config;
 
 import com.sp.fc.user.service.SpUserService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
@@ -11,14 +12,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
 
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final SpUserService userService;
+    private final DataSource dataSource;
 
-    public SecurityConfig(SpUserService userService) {
+    public SecurityConfig(SpUserService userService, DataSource dataSource) {
+        this.dataSource = dataSource;
         this.userService = userService;
     }
 
@@ -34,6 +45,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return roleHierarchy;
     }
     //관리자가 user 의 접근 권한도 가질 수 있도록 설정
+
+    @Bean
+    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
+        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher() {
+            @Override
+            public void sessionCreated(HttpSessionEvent event) {
+                super.sessionCreated(event);
+                System.out.println(">>>" + LocalDateTime.now() + "세션 생성됨 " + event.getSession().getId());
+            }
+
+            @Override
+            public void sessionDestroyed(HttpSessionEvent event) {
+                super.sessionDestroyed(event);
+                System.out.println(">>>" + LocalDateTime.now() + "세션 만료됨 " + event.getSession().getId());
+            }
+
+            @Override
+            public void sessionIdChanged(HttpSessionEvent event, String oldSessionId) {
+                super.sessionIdChanged(event, oldSessionId);
+                System.out.println(">>>" + LocalDateTime.now() + "세션 아이디 변경 " + oldSessionId + " -> " + event.getSession().getId());
+            }
+        });
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -52,7 +86,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 )
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .exceptionHandling(exception -> exception.accessDeniedPage("/access-denied"))
-                ;
+                .rememberMe(r->r
+                        .rememberMeServices(rememberMeServices()))
+        ;
+    }
+
+    @Bean
+    PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();
+        repository.setDataSource(dataSource);
+        try {
+            repository.removeUserTokens("1");
+        }catch (Exception e) {
+            repository.setCreateTableOnStartup(true);
+        }
+        return repository;
+    }
+
+    //remeber me service 방식 업데이트/ PersistentTokenBased 로.
+    @Bean
+    PersistentTokenBasedRememberMeServices rememberMeServices() {
+        PersistentTokenBasedRememberMeServices services =
+                new PersistentTokenBasedRememberMeServices("hello",
+                        userService, tokenRepository());
+        return services;
     }
 
     @Override
